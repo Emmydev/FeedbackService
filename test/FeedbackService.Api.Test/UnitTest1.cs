36e270123e1fb9410f073009cea90c56f4f3008b
Olayinka Emmanuel <olayinkaemmanuel52@gmail.com>
2022-06-18 16:13:20 +0100

    Project structure added

---
+++ test/FeedbackService.Api.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FeedbackServiceApi.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ test/FeedbackService.Core.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FeedbackService.Core.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
